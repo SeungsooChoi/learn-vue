49b1eab4ac81b5f27b00c5fa4c3b13d553d6c4b1
SeungsooChoi <tmdtn5389@gmail.com>
2022-03-07 23:58:56 +0900

    feat : ToDoList 연습

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <template>
   <div id="app">
     <h1>TodoList</h1>
+    <ul>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item
+          :label="item.label"
+          :done="item.done"
+          :id="item.id"
+        ></to-do-item>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import ToDoItem from "./components/ToDoItem.vue";
+
 export default {
   name: "App",
+  components: {
+    ToDoItem,
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { id: 1, label: "Learn Vue", done: false },
+        { id: 2, label: "Create a Vue project with the CLI", done: true },
+        { id: 3, label: "Have fun", done: true },
+        { id: 4, label: "Create a to-do list", done: false },
+      ],
+    };
+  },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
