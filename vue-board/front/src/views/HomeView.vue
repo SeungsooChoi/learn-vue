7925adb53150fb910bc775647467fa1311cb55f7
SeungsooChoi <tmdtn5389@gmail.com>
2022-03-17 17:10:06 +0900

    feat : axios 추가 후 db connect 테스트

--- vue-board/front/src/views/HomeView.vue
+++ vue-board/front/src/views/HomeView.vue
@@ -1,7 +1,18 @@
 <template>HomeView</template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  async beforeMount() {
+    try {
+      const response = await axios.get(`http://localhost:4000/`);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
 </script>
 
 <style></style>
