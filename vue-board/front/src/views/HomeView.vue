0f832f834660840d4ed2832f1c7e65391a1fdff5
SeungsooChoi <tmdtn5389@gmail.com>
2022-03-18 23:40:25 +0900

    feat : 간단한 create, read 테스트

--- vue-board/front/src/views/HomeView.vue
+++ vue-board/front/src/views/HomeView.vue
@@ -1,13 +1,33 @@
-<template>HomeView</template>
+<template>
+  <h1>HomeView</h1>
+  <ul class="postBlock">
+    <li v-for="post in state.posts" :key="post" class="postBlockItem">
+      <h1>{{ post.title }}</h1>
+      <p>{{ post.content }}</p>
+    </li>
+  </ul>
+</template>
 
 <script>
 import axios from "axios";
+import { reactive } from "vue";
 
 export default {
+  setup() {
+    const state = reactive({
+      posts: [],
+    });
+
+    return { state };
+  },
+
   async beforeMount() {
+    const RESPONSE_SUCCESS = "OK";
     try {
       const response = await axios.get(`http://localhost:4000/`);
-      console.log(response);
+      if (response.status === 200 && response.statusText === RESPONSE_SUCCESS) {
+        this.state.posts = response.data;
+      }
     } catch (error) {
       console.log(error);
     }
--- vue-board/front/src/views/PostView.vue
+++ vue-board/front/src/views/PostView.vue
@@ -1,7 +1,68 @@
-<template>PostView</template>
+<template>
+  <h1>Post</h1>
+  <form @submit.prevent="onSubmit">
+    <input
+      type="text"
+      placeholder="Title"
+      :value="state.title"
+      @input="updateTitle"
+    />
+    <textarea
+      maxlength="1000"
+      placeholder="content"
+      @input="updateContent"
+    ></textarea>
+    <button type="submit">Submit</button>
+  </form>
+</template>
 
 <script>
-export default {};
+import { reactive } from "vue";
+import axios from "axios";
+import router from "@/router";
+
+export default {
+  setup() {
+    const state = reactive({
+      title: "",
+      content: "",
+    });
+
+    const onSubmit = async () => {
+      const RESPONSE_SUCCESS = "OK";
+      const requestState = {
+        title: state.title,
+        content: state.content,
+      };
+
+      try {
+        const response = await axios.post(
+          `http://localhost:4000/post`,
+          requestState
+        );
+        if (
+          response.status === 200 &&
+          response.statusText === RESPONSE_SUCCESS
+        ) {
+          // redirect home
+          router.push("/");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const updateTitle = (e) => {
+      state.title = e.target.value;
+    };
+
+    const updateContent = (e) => {
+      state.content = e.target.value;
+    };
+
+    return { state, onSubmit, updateTitle, updateContent };
+  },
+};
 </script>
 
 <style></style>
